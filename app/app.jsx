0058a7cbf02540d5f8245a9349b1629d9f1518e6
Guangxian Piao <guangxian.piao@aexp.com>
2017-08-14 14:57:44 -0700

    Add Navigation

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 //Load foundation: use loaders 'style!css!' to load foundation.min.css
@@ -12,8 +14,10 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
-      <Route path="/" component={Main}>
-      </Route>
+    <Route path="/" component={Main}>
+      <Route path="countdown" component={Countdown}/>
+      <IndexRoute component={Timer}/>
+    </Route>
   </Router>,
   document.getElementById('app')
 );
